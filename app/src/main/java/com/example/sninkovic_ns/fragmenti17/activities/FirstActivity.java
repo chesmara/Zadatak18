a2eca32cc4a0d1fcd9a3be228c9ec1232812f774
chesmara <srdjan@ekoneimar.com>
2017-02-13 13:05:24 +0100

    sa posla

--- app/src/main/java/com/example/sninkovic_ns/fragmenti17/activities/FirstActivity.java
+++ app/src/main/java/com/example/sninkovic_ns/fragmenti17/activities/FirstActivity.java
@@ -2,23 +2,93 @@ package com.example.sninkovic_ns.fragmenti17.activities;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.sninkovic_ns.fragmenti17.Fragments.DetailFragment;
 import com.example.sninkovic_ns.fragmenti17.Fragments.MasterFragment;
+import com.example.sninkovic_ns.fragmenti17.Modeli.NavigationItem;
 import com.example.sninkovic_ns.fragmenti17.R;
+import com.example.sninkovic_ns.fragmenti17.adapters.DrawerAdapter;
+import com.example.sninkovic_ns.fragmenti17.dialogs.AboutDialog;
+
+import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity implements MasterFragment.OnItemSelectedListener {
 
 
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItemFromDrawer(position);
+        }
+    }
+
+
+
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
+    private RelativeLayout drawerPane;
+    private CharSequence drawerTitle;
+    private ArrayList<NavigationItem> drawerItems = new ArrayList<NavigationItem>();
+
+    private AlertDialog dialog;
+
+    // Attributes representing the activity's state
+    private boolean landscapeMode = false; // Is the device in the landscape mode?
+    private boolean masterShown = false; // Is the MasterFragment fragment shown?
+    private boolean detailShown = false; // Is the DetailFragment fragment shown?
+
+    private int itemId = 0; // Selected item ID
+
+
+
+
+
     boolean landscape=false;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        // Populates a list of NavigationDrawer items
+        drawerItems.add(new NavigationItem(getString(R.string.drawer_home), getString(R.string.drawer_home_long), R.drawable.ic_action_product));
+        drawerItems.add(new NavigationItem(getString(R.string.drawer_settings), getString(R.string.drawer_settings_long), R.drawable.ic_action_settings));
+        drawerItems.add(new NavigationItem(getString(R.string.drawer_about), getString(R.string.drawer_about_long), R.drawable.ic_action_about));
+
+        drawerTitle = getTitle();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerList = (ListView) findViewById(R.id.navList);
+
+        // Populates NavigtionDrawer with options
+        drawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+        DrawerAdapter adapter = new DrawerAdapter(this, drawerItems);
+
+        // Sets a custom shadow that overlays the main content when NavigationDrawer opens
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        drawerList.setAdapter(adapter);
 
 
 
@@ -28,10 +98,28 @@ public class FirstActivity extends AppCompatActivity implements MasterFragment.O
 
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_action_name);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_action_create);
             actionBar.setHomeButtonEnabled(true);
             actionBar.show();
         }
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                           /* host Activity */
+                drawerLayout,                   /* DrawerLayout object */
+                toolbar,                        /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,           /* "open drawer" description for accessibility */
+                R.string.drawer_close           /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(drawerTitle);
+                invalidateOptionsMenu();        // Creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(drawerTitle);
+                invalidateOptionsMenu();        // Creates call to onPrepareOptionsMenu()
+            }
+        };
 
 
 
@@ -40,7 +128,7 @@ public class FirstActivity extends AppCompatActivity implements MasterFragment.O
 
             FragmentTransaction ft = getFragmentManager().beginTransaction();
             MasterFragment masterFragment = new MasterFragment();
-            ft.add(R.id.list_view, masterFragment, "Master_Fragment_1");
+            ft.add(R.id.master_view, masterFragment, "Master_Fragment_1");
             ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
             ft.commit();
         }
@@ -56,8 +144,13 @@ public class FirstActivity extends AppCompatActivity implements MasterFragment.O
                 ft.replace(R.id.detail_view, detailFragment, "Detail_Fragment_1");
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 ft.commit();
+                detailShown = true;
             }
         }
+
+        masterShown = true;
+        detailShown = false;
+        itemId = 0;
 
 
     }
@@ -95,25 +188,110 @@ public class FirstActivity extends AppCompatActivity implements MasterFragment.O
 
 
 
-    public void onItemSelected(int position) {
-
+    public void onItemSelected(int id) {
+            itemId=id;
 
 
         if (landscape) {
             // If the device is in the landscape mode updates detail fragment's content.
             DetailFragment detailFragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.detail_view);
-            detailFragment.updateContent(position);
+            detailFragment.updateContent(id);
         } else {
             // If the device is in the portrait mode sets detail fragment's content and replaces master fragment with detail fragment in a fragment transaction.
             DetailFragment detailFragment = new DetailFragment();
-            detailFragment.setContent(position);
+            detailFragment.setContent(id);
             FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.replace(R.id.list_view, detailFragment, "Detail_Fragment_2");
+            ft.replace(R.id.master_view, detailFragment, "Detail_Fragment_2");
             ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
             ft.addToBackStack(null);
             ft.commit();
         }
     }
+
+    private void selectItemFromDrawer(int position) {
+
+        if (position == 0) {
+            // FirstActivity is already shown
+        } else if (position == 1) {
+            Intent settings = new Intent(FirstActivity.this,SettingsActivity.class);
+            startActivity(settings);
+        } else if (position == 2) {
+            if (dialog == null){
+                dialog = new AboutDialog(FirstActivity.this).prepareDialog();
+            } else {
+                if (dialog.isShowing()) {
+                    dialog.dismiss();
+                }
+            }
+
+            dialog.show();
+        }
+
+        drawerList.setItemChecked(position, true);
+        setTitle(drawerItems.get(position).getTitle());
+        drawerLayout.closeDrawer(drawerPane);
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (landscapeMode) {
+            finish();
+        } else if (masterShown == true) {
+            finish();
+        } else if (detailShown == true) {
+            getFragmentManager().popBackStack();
+            MasterFragment masterFragment = new MasterFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.master_view, masterFragment, "Master_Fragment");
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+            masterShown = true;
+            detailShown = false;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                Toast.makeText(this, "Action " + getString(R.string.fragment_master_action_create) + " executed.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_update:
+                Toast.makeText(this, "Action " + getString(R.string.fragment_detal_action_update) + " executed.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_delete:
+                Toast.makeText(this, "Action " + getString(R.string.fragment_detal_action_delete) + " executed.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        getSupportActionBar().setTitle(title);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration configuration) {
+        super.onConfigurationChanged(configuration);
+
+        // Pass any configuration change to the drawer toggle
+        drawerToggle.onConfigurationChanged(configuration);
+    }
+
+
+
+
+
 
 
 
